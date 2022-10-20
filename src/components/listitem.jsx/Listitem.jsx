9ceb981231b2a6c31fe69a00d20cf9387e750511
Affanzuhaib <affanzuhaibuddin@gmail.com>
2022-10-21 02:00:51 +0530

    hover effect

--- src/components/listitem.jsx/Listitem.jsx
+++ src/components/listitem.jsx/Listitem.jsx
@@ -1,16 +1,25 @@
 import { Add, PlayArrow, ThumbDownOutlined, ThumbUpOutlined } from "@material-ui/icons"
+import { useState } from "react"
 import "./listitem.scss"
 
-export default function Listitem() {
+export default function Listitem({index}) {
+  const [isHovered, setIsHovered]= useState(false);
+  const trailer="https://player.vimeo.com/external/496797033.sd.mp4?s=5ed472674c6878067353d85224650f6bc4f4a247&profile_id=164&oauth2_token_id=57447761";
   return (
-    <div className='listItem'>
+    <div className='listItem' 
+    style={{left : isHovered && index * 225-50 + index * 2.5}}
+    onMouseEnter={()=>setIsHovered(true)} 
+    onMouseLeave={()=>setIsHovered(false)}>
         <img src="https://media.istockphoto.com/vectors/cinema-poster-with-cola-filmstrip-and-clapper-vector-vector-id1244034031?k=20&m=1244034031&s=612x612&w=0&h=WFpMBZ7PfLCJrK4F9764OsAls0NbOJOza8CIiP3Yfc8=" alt="" />
+      {isHovered && (
+        <>
+        <video src={trailer} autoPlay={true} loop />
         <div className="itemInfo">
             <div className="icons">
-              <PlayArrow/>
-              <Add/>
-              <ThumbUpOutlined/>
-              <ThumbDownOutlined/>
+              <PlayArrow className="icon"/>
+              <Add className="icon"/>
+              <ThumbUpOutlined className="icon"/>
+              <ThumbDownOutlined className="icon"/>
             </div>
             <div className="itemInfoTop">
               <span>1 hourm14 mins</span>
@@ -22,6 +31,8 @@ export default function Listitem() {
             </div>
             <div className="genre">Action</div>
         </div>
+        </>
+        )}
     </div>
   )
 }
